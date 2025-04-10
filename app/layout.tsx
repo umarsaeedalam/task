e1e0965eee4dd4d7bdda75c6a281b4afd2994394
Umar Saeed <umarsaeedalam@gmail.com>
2025-04-10 14:09:07 +0500

    chore(setup): initial project configuration
    - Format code in layout.tsx
    - Update metadata to match company branding
    - Add company fonts to Mantine theme.ts
    - Set company icon as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
 import "@mantine/core/styles.css";
 import React from "react";
-import {
-  MantineProvider,
-  ColorSchemeScript,
-  mantineHtmlProps,
-} from "@mantine/core";
+import { MantineProvider, ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
 import { theme } from "../theme";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
+export const metadata: Metadata = {
+    title: {
+        template: 'PostNitro: %s',
+        default: 'PostNitro'
+    },
+    description: 'AI-Powered Carousel Generator for Instagram, LinkedIn & More',
 };
 
 export default function RootLayout({ children }: { children: any }) {
-  return (
-    <html lang="en" {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </head>
-      <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" {...mantineHtmlProps}>
+            <head>
+                <ColorSchemeScript />
+
+                <link rel="shortcut icon" href="/favicon.svg" />
+
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
+            </head>
+            <body>
+                <MantineProvider theme={theme}>
+                    {children}
+                </MantineProvider>
+            </body>
+        </html>
+    );
 }
--- theme.ts
+++ theme.ts
@@ -3,5 +3,7 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+    fontFamily: 'Inter, Inter Fallback, sans-serif',
+    fontFamilyMonospace: 'Disket Mono, Manrope, monospace',
+    headings: { fontFamily: 'Inter, Inter Fallback, sans-serif' },
 });
